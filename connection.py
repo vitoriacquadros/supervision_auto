803de6d7eda5b22ecfab867f871e5b4f6b337645
Vitória Quadros <104790491+vitoriacquadros@users.noreply.github.com>
2024-02-29 00:02:46 -0300

    update

---
+++ connection.py
@@ -0,0 +1,9 @@
+#criar uma conexão com o servidor MQTT
+# publicar mensagens em um tópico
+# inscrever-se em um tópico
+# receber mensagens de um tópico
+# desconectar do servidor MQTT
+# acionar o led via MQTT
+# desligar o led via MQTT
+
+class m
--- main.py
+++ main.py
@@ -1,22 +1,49 @@
 import tkinter as tk
+from PIL import Image, ImageTk
+import paho.mqtt.client as mqtt
 
-root = tk.Tk() #cria uma janela principal 
+class SystemSupervisory:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Sistema Supervisório")
+        
+        self.image = Image.open("the_flash_png_by_superflashofficial_de36frb-fullview.png")
+        self.image = ImageTk.PhotoImage(self.image)
+        
+        self.label_image = tk.Label(self.root, image=self.image, width=900, height=300)
+        self.label_image.pack()
+        
+        self.button_exit = tk.Button(self.root, text="Exit", command=self.root.quit)
+        self.button_exit.pack()
+        
+        self.button_start_click = tk.Button(self.root, text="Acionar LED", command=self.mqtt_publish)
+        self.button_start_click.pack()
+        
+        # Configurações MQTT para se conectar ao ESP32
+        self.mqtt_broker = "mqtt.eclipse.org"
+        self.mqtt_port = 1883
+        self.mqtt_topic = "esp32/led"
+        
+        # self.client = mqtt.Client()
+        # self.client.on_connect = self.on_connect
+        # self.client.connect(self.mqtt_broker, self.mqtt_port, 60)
+        # self.client.loop_start()
 
-#nome para janela principal 
+    def on_connect(self, client, userdate, flags, rc):
+        print("Conectado ao servidor MQTT")
+        self.client.subscribe(self.mqtt_topic)
+        print("Inscrito no tópico {}".format(self.mqtt_topic))
 
-root.title("Sistema Supervisório")
+    def mqtt_publish(self):
+        message = "TOGGLE"
+        self.client.publish(self.mqtt_topic, message)
+        
+    def button_click(self):
+        print("Botão clicado")
+        
+        
 
-#cria um rótulo
-
-label = tk.Label(root, text='Sistema Supervisório em Python')
-label.pack() #coloca o rótulo na janela
-
-#cria um botão
-
-button = tk.Button(root, text="Click me!")
-
-button.pack() #coloca o botão na janela
-
-#inicia o loop de eventos
-
-root.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = SystemSupervisory(root)
+    root.mainloop()
